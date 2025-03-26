1b34734616cffe83793b87882d00d1076ac2a8ee
seneall <vlad_palamarchuk_01@mail.ru>
2025-03-26 19:50:27 +0300

    add forwarded support; update blur logic

--- vk.user.js
+++ vk.user.js
@@ -1,60 +1,130 @@
 // ==UserScript==
-// @name        Vk.com
+// @name        seneal-vk-plus
 // @namespace   Violentmonkey Scripts
 // @match       https://vk.com/im*
 // @grant       none
-// @version     1.0
-// @author      -
+// @version     1.1
+// @author      seneal
 // ==/UserScript==
 
 const bluredUsers = [];
+const blurStrength = 5;
 
 const selectors = {
-  chat__message: "ConvoHistory__messageBlock",
+  page__body: "page_body",
+  chat__forward_new: "ForwardedMessageNew",
+  chat__message_header: "ConvoMessageHeader",
   chat__message_parent: "ConvoStack__content",
-  chat__message_author: "ConvoMessageHeader__authorLink",
+  chat__message: "ConvoHistory__messageWrapper",
+  chat__forward: "ForwardedMessagesList__content",
+  chat__forward_author: "ForwardedMessageNew__userName",
+  chat__message_author: "ConvoMessageWithoutBubble__avatar",
+  chat__message_buble_text: "ConvoMessageWithoutBubble__text",
+  chat__message_buble_media: "ConvoMessageWithoutBubble__mediaAttachments",
 };
 
-const handleNewMessage = (target) => {
-  const parent = target.closest("." + selectors.chat__message_parent);
-  const author = parent.querySelector("." + selectors.chat__message_author);
-  const href = author
-    .getAttribute("href")
-    .replace(/\/id\d+/, "")
-    .replace(/\//g, "");
+const isBluredUser = (href) => bluredUsers.includes(getUser(href));
 
-  if (!bluredUsers.includes(href)) return;
+const getUser = (href) => href.replace(/\/id\d+/, "").replace(/\//g, "");
 
-  target.classList.add("blur-text");
+const getClass = (name) => `.${name}`;
+
+const handleBlurElements = (user) => {
+  const elements = document.querySelectorAll(`[data-blur-user="${user}"]`);
+  elements.forEach((el) => el.classList.add("blur-text"));
+
+  elements.forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      elements.forEach((e) => e.classList.remove("blur-text"));
+    });
+
+    el.addEventListener("mouseleave", () => {
+      elements.forEach((e) => e.classList.add("blur-text"));
+    });
+  });
+};
+
+const handleForwardedMessage = (target) => {
+  const content = target.querySelector(getClass(selectors.chat__forward));
+  if (!content) return;
+
+  for (const element of content.children) {
+    if (!element.classList.contains(selectors.chat__forward_new)) continue;
+
+    const href = element.querySelector(getClass(selectors.chat__forward_author)).getAttribute("href");
+    if (!isBluredUser(href)) continue;
+
+    const user = getUser(href);
+
+    element.setAttribute("data-blur-user", user);
+    handleBlurElements(user);
+  }
+};
+
+const handleChatMessage = (target) => {
+  const parent = target.closest(getClass(selectors.chat__message_parent));
+  if (!parent) return;
+
+  const author = parent.querySelector(getClass(selectors.chat__message_author));
+  if (!author) return;
+
+  const href = author.getAttribute("href");
+  if (!href || !isBluredUser(href)) return;
+
+  const user = getUser(href);
+
+  author.setAttribute("data-blur-user", user);
+
+  const header = parent.querySelector(getClass(selectors.chat__message_header));
+  if (header) header.setAttribute("data-blur-user", user);
+
+  const bubles = [
+    ...parent.querySelectorAll(getClass(selectors.chat__message_buble_text)),
+    ...parent.querySelectorAll(getClass(selectors.chat__message_buble_media)),
+  ];
+
+  for (const buble of bubles) {
+    buble.setAttribute("data-blur-user", user);
+  }
+
+  handleBlurElements(user);
+};
+
+const startObserver = (selector, options, fn) => {
+  console.log(`Observing: ${selector}`);
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      const nearest = mutation.target.querySelector(selector);
+      if (nearest) fn(mutation.target);
+    }
+  });
+
+  const page = document.getElementById(selectors.page__body);
+
+  observer.observe(page, options);
 };
 
 const observers = [
   {
-    selector: selectors.chat__message,
-    options: { childList: true, subtree: true },
-    fn: handleNewMessage,
+    fn: handleChatMessage,
+    selector: getClass(selectors.chat__message),
+    options: { subtree: true, childList: true },
+  },
+  {
+    fn: handleForwardedMessage,
+    selector: getClass(selectors.chat__forward),
+    options: { subtree: true, childList: true },
   },
 ];
 
-const startObserver = (selector, options, fn) => {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.target.classList.contains(selector)) fn(mutation.target);
-    }
-  });
-
-  observer.observe(document.body, options);
-};
-
 const injectStyles = () => {
   const style = document.createElement("style");
-  style.textContent = `
-    .blur-text {filter: blur(6px);cursor: pointer;transition: filter 0.3s ease;}
-    .blur-text:hover {filter: none;transition: filter 0.3s ease;}
-    `;
+  style.textContent = `.blur-text {filter: blur(${blurStrength}px);cursor: pointer;transition: filter 0.3s ease;}.blur-text:hover {filter: none;transition: filter 0.3s ease;}`;
 
   document.head.appendChild(style);
 };
 
 injectStyles();
-observers.forEach(({ selector, options, fn }) => startObserver(selector, options, fn));
+
+observers.forEach(({ fn, options, selector }) => startObserver(selector, options, fn));
